4808f98197bea44847b00ac07d02645b33ad9bdf
AndresRiveroLedo <andresriveroformacion@gmail.com>
2022-07-21 20:34:10 +0200

    desktop-menu && mobile-menu

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
+/*Menu desplegable email */
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+/*Menu desplegable mobile */
+const menuHamIcon = document.querySelector('.menuHamIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+/*Menu desplegable carrito */
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const myOrderCard = document.querySelector('.product-detail')
+
+
+/*Eventos*/
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleMyOrderCard)
 
+/**Funciones */
 function toggleDesktopMenu(){
+    const isMyOrderCardOpen = !myOrderCard.classList.toggle('inactive');
+
+    if(isMyOrderCardOpen){
+        myOrderCard.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isMyOrderCardOpen = !myOrderCard.classList.toggle('inactive');
+
+    if(isMyOrderCardOpen){
+        myOrderCard.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMyOrderCard(){
+    const isMobileMenuOpen = !mobileMenu.classList.toggle('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.toggle('inactive');
+
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactive');
+   
+    }
+
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive');
+    }
+
+
+    myOrderCard.classList.toggle('inactive');
 }
