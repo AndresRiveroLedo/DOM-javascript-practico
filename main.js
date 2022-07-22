4d6f3e504fe6362625b17e6c57ffdf211d7c0347
AndresRiveroLedo <andresriveroformacion@gmail.com>
2022-07-22 21:59:07 +0200

    Card container product

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
 const myOrderCard = document.querySelector('.product-detail')
 
+const cardsContainer=document.querySelector('.cards-container');
+
+//Array de producto
+const product = [];
 
 /*Eventos*/
 navEmail.addEventListener('click', toggleDesktopMenu);
@@ -16,39 +20,114 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleMyOrderCard)
 
 /**Funciones */
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const isMyOrderCardOpen = !myOrderCard.classList.toggle('inactive');
 
-    if(isMyOrderCardOpen){
+    if (isMyOrderCardOpen) {
         myOrderCard.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const isMyOrderCardOpen = !myOrderCard.classList.toggle('inactive');
 
-    if(isMyOrderCardOpen){
+    if (isMyOrderCardOpen) {
         myOrderCard.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleMyOrderCard(){
+function toggleMyOrderCard() {
     const isMobileMenuOpen = !mobileMenu.classList.toggle('inactive');
     const isDesktopMenuOpen = !desktopMenu.classList.toggle('inactive');
 
-    if(isMobileMenuOpen){
+    if (isMobileMenuOpen) {
         mobileMenu.classList.add('inactive');
-   
+
     }
 
-    if(isDesktopMenuOpen){
+    if (isDesktopMenuOpen) {
         desktopMenu.classList.add('inactive');
     }
 
 
     myOrderCard.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+}, {
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},{
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProductList(arr) {
+    /*
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+            <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+            </figure>
+        </div>
+    </div>
+  */
+    for (const product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoCard = document.createElement('div');
+
+        const priceProduct = document.createElement('p');
+        priceProduct.innerText = '$' + product.price;
+        const nameProduct = document.createElement('p');
+        nameProduct.innerText = product.name;
+
+        const figureCart = document.createElement('figure');
+
+        const imgCart = document.createElement('img');
+        imgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+
+        figureCart.appendChild(imgCart);
+
+        productInfoCard.appendChild(priceProduct);
+        productInfoCard.appendChild(nameProduct);
+
+        productInfo.appendChild(productInfoCard);
+        productInfo.appendChild(figureCart);
+
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+        console.log('prueba1')
+
+    }
+
+}
+
+renderProductList(productList);
